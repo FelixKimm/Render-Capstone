53043cb12ce81820c5fc2983631e3df0c5d17b29
Scadaria 02 <scadaria02@iMac-de-Scadaria-2.local>
2023-11-17 22:50:04 -0300

    app finalizado

--- app.py
+++ app.py
@@ -1,29 +1,237 @@
 import os
-from flask import Flask
-from models import setup_db
+from flask import Flask, request, abort, jsonify, flash
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+# from .auth.auth import AuthError, requires_auth
+
+from models import setup_db, Team, Player
 
 def create_app(test_config=None):
-
     app = Flask(__name__)
     setup_db(app)
     CORS(app)
 
-    @app.route('/')
-    def get_greeting():
-        excited = os.environ['EXCITED']
-        greeting = "Hello" 
-        if excited == 'true': 
-            greeting = greeting + "!!!!! You are doing great in this Udacity project."
-        return greeting
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type, Authorization, true"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Headers", "GET, POST, PATCH, DELETE, OPTIONS"
+        )
+        return response
 
-    @app.route('/coolkids')
-    def be_cool():
-        return "Be cool, man, be coooool! You're almost a FSND grad!"
+    # TEAM ENDPOINTS
+
+    @app.route('/teams', methods=['GET'])
+    def get_teams():
+        teams = Team.query.all()
+
+        if len(teams) == 0:
+            return 'There are no teams on the database!'
+
+        formatted_teams = []
+        for team in teams:
+            team_data = team.format()
+            team_data['total_players'] = len(team.players)
+            formatted_teams.append(team_data)
+
+        return {
+            'success': True,
+            'Teams': formatted_teams
+        }
+    
+    @app.route('/teams', methods=['POST'])
+    def create_team():
+        body = request.get_json()
+
+        new_name = body.get('name')
+        
+        if not ('name' in body):
+            abort(422)
+        
+        try:
+            team = Team(name=new_name)
+
+            team.insert()
+
+            return jsonify ({
+                'sucess': True,
+                'team_id': team.id,
+                'team_name': team.name
+            })
+            
+        except:
+            abort(422)
+
+    @app.route('/teams/<int:team_id>', methods=['DELETE'])
+    def delete_team(team_id):
+        try:
+            team = Team.query.get(team_id)
+
+            if team is None:
+                print("This team id does't exist")
+                abort(404)
+
+            players_on_team = team.players
+            players_deleted = [player.name for player in players_on_team]
+
+            # Delete all players associated with a particular team before deleting the team
+            for player in players_on_team:
+                player.delete()
+
+            team.delete()
+
+            return jsonify({
+                'success': True,
+                'deleted_team': team.name,
+                'deleteed_players': players_deleted
+            })
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            abort(422)
+
+    @app.route('/teams/<int:team_id>', methods=['PATCH'])
+    def patch_team(team_id):
+        body = request.get_json()
+    
+        patch_team = Team.query.get(team_id)
+        old_name = patch_team.name
+
+        patch_name = body.get('name')
+
+        if patch_name is None:
+            abort(404)
+        
+        if patch_name:
+            patch_team.name = patch_name
+
+        patch_team.update()
+
+        return jsonify ({
+            'success': True,
+            'old_name': old_name,
+            'new_name': patch_name
+        }) 
+
+
+    # PLAYER ENDPOINTS
+
+    @app.route('/players', methods=['GET'])
+    def get_players():
+        players = Player.query.order_by(Player.team_id).all()
+
+        if len(players) == 0:
+            return 'There are no players on the database!'
+
+        formatted_players = [player.format() for player in players]
+
+        return {
+            'sucess': True,
+            'Players': formatted_players,
+            'total_players': len(players)
+        }
+
+
+    @app.route('/players', methods=['POST'])
+    def create_player():
+        body = request.get_json()
+
+        new_name = body.get('name')
+        team_id = body.get('team_id')
+
+        if not ('name' in body and 'team_id' in body):
+            abort(422)
+
+        team = Team.query.get(team_id)
+        if not team:
+            print('There is no team with this id')
+            abort(404)
+        try:
+            player = Player(name=new_name, team_id=team_id)
+
+            player.insert()
+
+            return jsonify({
+                'success': True,
+                'player_id': player.id,
+                'player_name': player.name,
+                'team_id': player.team_id
+            })
+        except:
+            abort(422)
+
+    @app.route('/players/<int:player_id>', methods=['DELETE'])
+    def delete_player(player_id):
+        try:
+            player = Player.query.get(player_id)
+
+            if player is None:
+                print("This player id does't exist")
+                abort(422)
+            
+            player.delete()
+
+            return jsonify ({
+                'success': True,
+                'deleted_player': player.name
+            })
+
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+            abort(422)
+
+    @app.route('/players/<int:player_id>', methods=['PATCH'])
+    def patch_player(player_id):
+        body = request.get_json()
+
+        patch_player = Player.query.get(player_id)
+        old_team = patch_player.team_id
+
+        patch_name = body.get('team_id')
+        patch_team_id = body.get('team_id')
+
+        if patch_player is None:
+            print("There is no team with this id")
+            abort(404)
+
+        if patch_team_id:
+            patch_player.team_id = patch_team_id
+
+        patch_player.update()
+
+        return jsonify ({
+            'success': True,
+            'old_team': old_team,
+            'new_team': patch_name
+        }) 
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({"success": False, "error": 400, "message": "bad request"}), 400
+
+    @app.errorhandler(401)
+    def bad_request(error):
+        return jsonify({"success": False, "error": 401, "message": "Unathorized"}), 401
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({"success": False, "error": 404, "message": "resource not found"}), 404
+
+    @app.errorhandler(405)
+    def not_allowed(error):
+        return jsonify({"success": False, "error": 405, "message": "method not allowed"}), 405
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({"success": False, "error": 422, "message": "unprocessable"}), 422
+
+    @app.errorhandler(500)
+    def server_error(error):
+        return jsonify({"success": False, "error": 500, "message": "server error"}), 500
+    
+    # @app.errorhandler(AuthError)
+    # def autherror(error):
+    #     return jsonify({"success": False, "error": 401, "message": "server error"}), 401
 
     return app
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run()
--- models.py
+++ models.py
@@ -2,42 +2,77 @@ import os
 from sqlalchemy import Column, String, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+from flask_migrate import Migrate
+from settings import DB_NAME, DB_USER, DB_PASSWORD
 
-database_path = os.environ['DATABASE_URL']
-if database_path.startswith("postgres://"):
-  database_path = database_path.replace("postgres://", "postgresql://", 1)
+database_name = DB_NAME
+database_path = f'postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5432/{DB_NAME}'
 
 db = SQLAlchemy()
 
-'''
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-'''
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["DEBUG"] = True
     db.app = app
     db.init_app(app)
-    db.create_all()
+    migrate = Migrate(app, db)
+    with app.app_context():
+        db.create_all()
 
+class Team(db.Model):
+    __tablename__='teams'
 
-'''
-Person
-Have title and release year
-'''
-class Person(db.Model):  
-  __tablename__ = 'People'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    players = db.relationship('Player', backref='team', lazy=True)
 
-  id = Column(db.Integer, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
+    def __init__(self, name):
+        self.name = name
 
-  def __init__(self, name, catchphrase=""):
-    self.name = name
-    self.catchphrase = catchphrase
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'catchphrase': self.catchphrase}
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+          'id': self.id,
+          'name': self.name,
+          'players': [player.format() for player in self.players]
+        }
+
+class Player(db.Model):
+    __tablename__='players'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
+
+    def __init__(self, name, team_id):
+        self.name = name
+        self.team_id = team_id
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'team_id': self.team_id
+        }
